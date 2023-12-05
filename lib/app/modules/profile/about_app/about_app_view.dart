6f7819c3d7e9dee84ac83746786c806ad1d6468d
Hasan-Alturk <hasanalturk2016@gmail.com>
2023-12-05 03:13:14 +0300

    hasan

--- lib/app/modules/profile/about_app/about_app_view.dart
+++ lib/app/modules/profile/about_app/about_app_view.dart
@@ -3,6 +3,8 @@ import 'package:black_market/app/core/constants/app_strings.dart';
 import 'package:black_market/app/core/widgets/custom_app_bar.dart';
 import 'package:black_market/app/modules/profile/about_app/about_app_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
 import 'package:get/get.dart';
 
 class AboutAppView extends GetView<AboutAppController> {
@@ -16,7 +18,34 @@ class AboutAppView extends GetView<AboutAppController> {
         text: AppStrings.aboutApp,
         onTap: () => Get.back(),
       ),
-      body: const SingleChildScrollView(),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 15.h,
+            ),
+            GetBuilder<AboutAppController>(
+              builder: (_){
+                return Expanded(
+                child: SingleChildScrollView(
+                  child: HtmlWidget(
+                    controller.text,
+                    textStyle: TextStyle(
+                      fontSize: 14.sp,
+                      color: AppColors.lighterGrey,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ),
+              ),
+              },
+            ),
+          ],
+
+          
+        ),
+      ),
       // body: SafeArea(
       //   child: Padding(
       //     padding: const EdgeInsets.all(10),
