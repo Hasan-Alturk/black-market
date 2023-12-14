3e67b89a236952146fa93896d3ae8cfa2989e1e6
emankhaled96 <emankhaled1196@gmail.com>
2023-12-14 22:11:33 +0200

    sorted_currencies

--- lib/app/modules/currencies/currencies_controller.dart
+++ lib/app/modules/currencies/currencies_controller.dart
@@ -150,8 +150,9 @@ class CurrenciesController extends GetxController {
     currencyInBankList.clear();
     for (var element in latestCurrencyList) {
       if (element.bankPrices != null) {
-        outerLoop:
-        for (var bank in element.bankPrices!) {
+        var x = element.bankPrices!.where((value) =>
+            DateTime.parse(value.updatedAt).day == DateTime.now().day);
+        for (var bank in x) {
           innerLoop:
           for (var b in bankList) {
             if (bank.currencyId == currencyId && b.id == bank.bankId) {
@@ -176,7 +177,7 @@ class CurrenciesController extends GetxController {
               }
               break innerLoop;
             } else {
-              continue ;
+              continue;
             }
           }
         }
--- lib/app/modules/currencies/currencies_view.dart
+++ lib/app/modules/currencies/currencies_view.dart
@@ -17,7 +17,7 @@ class CurrenciesView extends GetView<CurrenciesController> {
 
   @override
   Widget build(BuildContext context) {
-    // controller.onInit();
+    controller.onInit();
     return Scaffold(
         backgroundColor: AppColors.blackDarkActive,
         body: SafeArea(
