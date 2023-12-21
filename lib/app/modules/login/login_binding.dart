b9d73d82d4fada9a906f7e27833b8b5420cacc82
Hasan-Alturk <hasanalturk2016@gmail.com>
2023-12-21 23:42:27 +0300

    a

--- lib/app/modules/login/login_binding.dart
+++ lib/app/modules/login/login_binding.dart
@@ -1,7 +1,6 @@
 import 'package:black_market/app/core/repo/auth_repo.dart';
 import 'package:black_market/app/core/repo/setting_repo.dart';
 import 'package:black_market/app/modules/login/login_controller.dart';
-import 'package:black_market/app/modules/main/main_home_binding.dart';
 import 'package:dio/dio.dart';
 import 'package:get/get.dart';
 
